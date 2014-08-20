396328476bee8e8ce250bce7d8b93b46c3bc9be9
Jens W. Klein <jk@kleinundpartner.at>
2014-08-20 18:29:09 +0200

    minor housekeeping

---
+++ docs/HISTORY.rst
@@ -0,0 +1,55 @@
+Changelog
+=========
+
+1.8 (Unreleased)
+----------------
+- fix duplicated ID for accessibility [simahawk]
+- fix rendering under plone 4.3 [simahawk]
+- feat: add event and triggers for content rules [Gagaro]
+- qa: add travis and coverage support [toutp]
+- qa: pep8 fixes [toutp,Gagaro,jensens]
+
+1.7 (2013-06-04)
+----------------
+- Add french translation [toutpt]
+- Make the template being 'index' to be customizable with zcml + browser layer
+  [toutpt]
+- Add catalan translation [mpampols]
+
+1.6 (2012-06-11)
+----------------
+- Fix requirements for instances where dexterity is not installed [tschorr]
+
+1.5 (2012-06-07)
+----------------
+- Add support for dexterity behaviors [eleddy]
+- Fix confirmation message alignment for default plone sites [eleddy]
+
+1.4 (2012-05-15)
+----------------
+- German translation added. [jensens]
+- Added Brazilian Portuguese translation. [agnogueira]
+- Add support for Plone 3 [rochecompaan]
+- Enable use of the twotumbs widget outside the content div [rochecompaan]
+
+1.3 (2011-09-22)
+----------------
+- Turn thumbs background images into a sprite [marcosfromero]
+- Improve accessibility: non AJAX/JavaScript support [marcosfromero]
+- Improve i18n and added new Spanish translations [marcosfromero]
+- Improve feedback for anonymous users and after voting [marcosfromero]
+
+1.2 (2011-05-27)
+----------------
+- Initial i18n and Spanish translation [hvelarde]
+
+
+1.1 (2011-02-15)
+----------------
+- switch to absolute patch for form actions since it breaks when
+  under the influence of rewriting [eleddy]
+
+
+1 (2011-02-14)
+--------------
+- Initial release [eleddy on code, spanktar on graphics]
--- setup.py
+++ setup.py
@@ -2,36 +2,38 @@ from setuptools import setup, find_packages
 import os
 
 version = '1.7'
+description = open("README.rst").read() + "\n"
+description += open(os.path.join("docs", "HISTORY.rst")).read()
 
-setup(name='cioppino.twothumbs',
-      version=version,
-      description="Rating widget based on thumbs up and down.",
-      long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+setup(
+    name='cioppino.twothumbs',
+    version=version,
+    description="Rating widget based on thumbs up and down.",
+    long_description=description,
+    # Get more strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
-        ],
-      keywords='rating, content, thumbs',
-      author='eleddy',
-      author_email='elizabeth.leddy@gmail.com',
-      url='https://github.com/eleddy/cioppino.twothumbs',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['cioppino'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'plone.behavior',
-      ],
-      extras_require={'test': ['plone.app.testing'] },
-      entry_points="""
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      setup_requires=[],
-
-      )
+    ],
+    keywords='rating, content, thumbs',
+    author='eleddy',
+    author_email='elizabeth.leddy@gmail.com',
+    url='https://github.com/eleddy/cioppino.twothumbs',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['cioppino'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Plone',
+        'plone.behavior',
+        'setuptools',
+    ],
+    extras_require={'test': ['plone.app.testing']},
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+    setup_requires=[],
+)
