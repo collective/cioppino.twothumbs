fd8f4b045d7b13716b4a645afc2dc651fb1da1c2
Philip Bauer <bauer@starzel.de>
2016-03-09 15:04:08 +0100

    Back to development: 2.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '2.0'
+version = '2.1.dev0'
 description = open("README.rst").read() + "\n"
 description += open(os.path.join("docs", "HISTORY.rst")).read()
 
