3bdb6e211db2cf58cc53fd3b1b246af203227787
Gil Forcada <gil.gnome@gmail.com>
2019-01-05 21:53:55 +0100

    Back to development: 2.1.3
    [ci skip]

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '2.1.2'
+version = '2.1.3.dev0'
 description = open("README.rst").read() + "\n"
 description += open("CHANGES.rst").read()
 
