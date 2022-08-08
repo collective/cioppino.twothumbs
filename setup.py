7093957f2eeb95a296df5e1918096d8c0692f808
Maurits van Rees <maurits@vanrees.org>
2022-08-08 17:00:45 +0200

    Back to development: 2.2.6
    [ci skip]

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '2.2.5'
+version = '2.2.6.dev0'
 description = open("README.rst").read() + "\n"
 description += open("CHANGES.rst").read()
 
